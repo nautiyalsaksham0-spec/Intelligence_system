1c6d6f6c24e8d6e1f6f0ed13bf3965095504dd0d
Saksham Nautiyal <nautiyalsaksham0@gmail.com>
2026-07-07 17:23:01 +0530

    in day 1 created data ingestion module

---
+++ ingest.py
@@ -0,0 +1,27 @@
+import requests
+import xml.etree.ElementTree as ET
+def fetch_live_events():
+    print("Initiating Data Ingestion Pipeline...")
+    url = "http://feeds.bbci.co.uk/news/world/rss.xml"
+    response = requests.get(url)
+    if response.status_code==200:
+        print("Success: Connected to live feed!\n")
+        root = ET.fromstring(response.content)
+        events = []
+        for item in root.findall('.//item')[:5]:
+            title = item.find('title').text
+            description = item.find('description').text
+            events.append({
+                "report_title": title, 
+                "details": description
+            })
+        return events
+    else:
+        print(f"CRITICAL: Failed to fetch data. Status code: {response.status_code}")
+        return []
+if __name__ == "__main__":
+    raw_intelligence_data = fetch_live_events()
+    for i,event in enumerate(raw_intelligence_data,1):
+        print(f"--- INCOMING EVENT {i} ---")
+        print(f"REPORT : {event['report_title']}")
+        print(f"DETAILS: {event['details']}\n")
